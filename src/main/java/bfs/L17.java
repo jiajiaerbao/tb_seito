8fc00aad8c92a39adebaa6b3bb4b004823158705
seito <ryuuseitou@gmal.com>
2018-11-26 21:58:34 -0800

    这道题耽误了四个小时, 不明白的话, 多听视频!!!! Everyday is the last day!

---
+++ src/main/java/bfs/L17.java
@@ -0,0 +1,91 @@
+package bfs;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class L17 {
+    /*
+    * 这道题用定长搜索的模板: 还是不熟练
+    * 对于模板而言:
+    *   queue: res
+    *   pathLength: digits.length
+    *   size: res.size()
+    *   converts(cur): get(digits.charAt(i))
+    * */
+    public List<String> letterCombinations(String digits) {
+        List<String> res = new LinkedList<>();
+        //corner case
+        if (digits.length() == 0) {
+            return res;
+        }
+        //这里只是让res开一个头
+        res.add("");
+        //pathLength: digits的length: 这里定长的意思就是最后输出的结果的长度
+        for (int i = 0; i < digits.length(); i++) {
+            int size = res.size();
+            //对queue里面的每一个元素, 循环取出第一个元素, 然后添加一个字母后, 再加到 res 的末尾
+            //到底添加多少个字母是由外层的for(pathLength)来决定的
+            for (int k = 0; k < size; k++) {
+                String cur = res.get(0);
+                res.remove(0);
+                List<Character> list = get(digits.charAt(i));
+                //这里是next=convert(cur)
+                for (char ch : list) {
+                    res.add(cur + ch);
+                }
+            }
+        }
+        return res;
+    }
+
+
+    public List<Character> get(char c) {
+        List<Character> res = new ArrayList<>();
+        switch (c) {
+            case '2':
+                res.add('a');
+                res.add('b');
+                res.add('c');
+                break;
+            case '3':
+                res.add('d');
+                res.add('e');
+                res.add('f');
+                break;
+            case '4':
+                res.add('g');
+                res.add('h');
+                res.add('i');
+                break;
+            case '5':
+                res.add('j');
+                res.add('k');
+                res.add('l');
+                break;
+            case '6':
+                res.add('m');
+                res.add('n');
+                res.add('o');
+                break;
+            case '7':
+                res.add('p');
+                res.add('q');
+                res.add('r');
+                res.add('s');
+                break;
+            case '8':
+                res.add('t');
+                res.add('u');
+                res.add('v');
+                break;
+            case '9':
+                res.add('w');
+                res.add('x');
+                res.add('y');
+                res.add('z');
+                break;
+        }
+        return res;
+    }
+}
