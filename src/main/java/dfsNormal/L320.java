ac0c20e6333ef9d7c8924c7d92d96b5583fda0de
seito <ryuuseitou@gmal.com>
2018-12-04 10:08:51 -0800

    补充一个点 Everyday is the last day!

--- src/main/java/dfsNormal/L320.java
+++ src/main/java/dfsNormal/L320.java
@@ -7,6 +7,7 @@ public class L320 {
     /*
     * 坑 1: base case success: 在idx最后走到string的末尾的时候, 记得把剩下的lastVal加到path里面, 然后再保存进res里面
     * 坑 2: set back: 在把lastVal加入到path, path保存进res里面之后, 记得把path的长度恢复到原来的状态, 这样返回上一层的时候, 才不会改变上一层path的状态
+    * 坑 3: 在base case success的里面, 只有当 lastVal 大于 0 的时候, 才添加到 path 里面
     * */
     public List<String> generateAbbreviations(String word) {
         List<String> res = new ArrayList<>();
