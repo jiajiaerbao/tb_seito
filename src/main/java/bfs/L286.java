7f5d788db385b74c4d07e5bc8060260e637f138b
seito <ryuuseitou@gmal.com>
2018-12-08 19:05:49 -0800

    第二遍还是做不对, 证明你第一遍无论做得多慢效果都是一样的, 所以要是要快点做 Everyday is the last day!

--- src/main/java/bfs/L286.java
+++ src/main/java/bfs/L286.java
@@ -69,4 +69,99 @@ public class L286 {
         }
         return res;
     }
+
+
+    /****************************************第二遍*******************************************/
+
+    /*
+    * 坑 0: 这里需要跟面试官clarify: throw new IllegalArgumentException("Invalid Input!");
+    * 坑 1: 这里是把所有的零都放入到Queue里面
+    * 坑 2: 改变 某个Cell的距离 的时机 是 touch 到了这个Cell, 但是在放入Queue之前
+    * 坑 3: 最大值和最小值要区分开
+    * */
+
+
+    public void wallsAndGates2(int[][] rooms) {
+        //corner case
+        if(rooms == null || rooms.length == 0 || rooms[0] == null || rooms[0].length == 0){
+            return;
+        }
+        int row = rooms.length;
+        int col = rooms[0].length;
+        Queue<int[]> que = new LinkedList<>();
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                if(rooms[i][j] == 0){
+                   que.offer(new int[]{i, j});
+                }
+            }
+        }
+        int len = 1;
+        int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        while (!que.isEmpty()){
+            int size = que.size();
+            while (size-- > 0){
+                int[] cur = que.poll();
+                for(int[] dir : directions){
+                    int nextI = cur[0]+dir[0];
+                    int nextJ = cur[1]+dir[1];
+                    if(nextI >= 0 && nextI < row
+                    && nextJ >= 0 && nextJ < col
+                    && rooms[nextI][nextJ] == Integer.MAX_VALUE){
+                        que.offer(new int[]{nextI, nextJ});
+                        rooms[nextI][nextJ] = len;
+                    }
+                }
+            }
+            len++;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
