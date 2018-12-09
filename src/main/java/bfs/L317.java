7725c425b49a69dc999de788fd05d8aa55b8e2b5
seito <ryuuseitou@gmal.com>
2018-12-08 20:32:34 -0800

    第二遍做了一个小时, 你还是不熟练, 有许多坑还是没搞懂 Everyday is the last day!

--- src/main/java/bfs/L317.java
+++ src/main/java/bfs/L317.java
@@ -90,4 +90,123 @@ public class L317 {
         return res;
     }
 
+
+
+
+
+    /****************************************第二遍*******************************************/
+    /*
+    * 坑 1: 在touch到某个点之后, 放入到Queue里面之前, 一定要设置该Cell的Visited为true
+    * 坑 2: 千万不要忘记放到Queue里面
+    * 坑 3: min的最后结果如果还是MAX_VALUE的话, 说明题目要求的节点不存在, 这时需要跟面试官确认该如何处理, 比如返回-1
+    * 坑 4: post processing 忘记了
+    * 坑 5: 最后在确定最小距离的两层for loop里面, 是grid[i][j] == 0, 是基于 Input 的数值来考虑的
+    * */
+
+
+    public int shortestDistance2(int[][] grid) {
+        //corner case
+        if(grid == null || grid.length == 0 || grid[0] == null || grid[0].length == 0){
+            throw new IllegalArgumentException("Invalid Input");
+        }
+
+        int row = grid.length;
+        int col = grid[0].length;
+
+        int[][] disc = new int[row][col];
+
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                if(grid[i][j] == 1){
+                    bfs(grid, disc, i, j, row, col);
+                }
+            }
+        }
+
+        int min = Integer.MAX_VALUE;
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                if(grid[i][j] == 0) {
+                    min = Math.min(min, disc[i][j]);
+                }
+            }
+        }
+        return min == Integer.MAX_VALUE ? -1: min;
+    }
+
+    private void bfs(int[][] grid, int[][] disc, int idx, int jdx, int row, int col){
+        int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        boolean[][] visited = new boolean[row][col];
+        Queue<int[]> que = new LinkedList<>();
+        que.offer(new int[]{idx, jdx});
+        //visited[idx][jdx] = true; //这句话多余
+        int len = 1;
+        while (!que.isEmpty()){
+            int size = que.size();
+            while (size-- > 0){
+                int[] cur = que.poll();
+                for(int[] dir : directions){
+                    int nextI = cur[0] + dir[0];
+                    int nextJ = cur[1] + dir[1];
+                    if(nextI >= 0 && nextI < row
+                    && nextJ >= 0 && nextJ < col
+                    && grid[nextI][nextJ] == 0
+                    && !visited[nextI][nextJ]){
+                        que.offer(new int[]{nextI, nextJ});
+                        visited[nextI][nextJ] = true;
+                        disc[nextI][nextJ] += len;
+                    }
+                }
+            }
+            len++;
+        }
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                if(!visited[i][j] && grid[i][j] == 0){
+                    grid[i][j] = 2 ;
+                }
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
