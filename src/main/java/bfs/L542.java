b1d63f70851b9326fb8f55a3886b4b12b94c6879
seito <ryuuseitou@gmal.com>
2018-12-08 19:29:28 -0800

    第二遍做的时候, 查环的地方还是不太熟练 Everyday is the last day!

--- src/main/java/bfs/L542.java
+++ src/main/java/bfs/L542.java
@@ -65,4 +65,97 @@ public class L542 {
         }
         return res;
     }
+
+
+    /****************************************第二遍*******************************************/
+    /*
+    *   坑 1: 查环忘记了, 这里的查环跟第一种解法的查环还是不太一样,
+    *           第一种解法是根据原matrix和结果matrix结合起来一起考虑: 只有值为 1 的节点, 并且距离为 0 (未 touch 过的节点才会被访问)
+    *           第二种解法是单独定义了一个boolean数组来表示这个节点是否被touch过
+    * */
+
+
+    public int[][] updateMatrix2(int[][] matrix) {
+        //corner case
+        if(matrix == null || matrix.length == 0 || matrix[0] == null || matrix[0].length == 0){
+            throw new IllegalArgumentException("Invalid input");
+        }
+
+        int row = matrix.length;
+        int col = matrix[0].length;
+
+        int[][] res = new int[row][col];
+        boolean[][] visited = new boolean[row][col];
+
+        Queue<int[]> que = new LinkedList<>();
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                if(matrix[i][j] == 0){
+                    res[i][j] = 0;
+                    visited[i][j] = true;
+                    que.offer(new int[]{i, j});
+                }
+            }
+        }
+        int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        int len = 1;
+        while (!que.isEmpty()){
+            int size = que.size();
+            while (size-- > 0){
+                int[] cur = que.poll();
+                for(int[] dir : directions){
+                    int nextI = cur[0] + dir[0];
+                    int nextJ = cur[1] + dir[1];
+                    if(nextI >= 0 && nextI < row
+                    && nextJ >= 0 && nextJ < col
+                    && matrix[nextI][nextJ] == 1 && !visited[nextI][nextJ]){
+                        res[nextI][nextJ] = len;
+                        visited[nextI][nextJ] = true;
+                        que.offer(new int[]{nextI, nextJ});
+                    }
+                }
+            }
+            len++;
+        }
+        return res;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
