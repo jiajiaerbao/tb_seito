df427c91e3c3f02100add7d775cd07eff5f3685c
seito <ryuuseitou@gmal.com>
2018-12-10 20:02:48 -0800

    这道题是自己做出来的, 因为前面的相同题目的hard看了答案了, 所以这道中等难度的题目能很快做出来 Everyday is the last day!

---
+++ src/main/java/dfsPruning/L139.java
@@ -0,0 +1,37 @@
+package dfsPruning;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class L139 {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        if (s == null || s.length() == 0 || wordDict == null || wordDict.size() == 0) {
+            return false; //or throw Exception
+        }
+        Set<String> dict = new HashSet<>();
+        for (String word : wordDict) {
+            dict.add(word);
+        }
+        Boolean[] mem = new Boolean[s.length() + 1];
+        return search(s, dict, 0, mem);
+    }
+
+    private boolean search(String s, Set<String> dict, int idx, Boolean[] mem) {
+        if (idx == s.length()) {
+            return true;
+        }
+        if (mem[idx] != null) {
+            return mem[idx];
+        }
+        for (int i = idx + 1; i <= s.length(); i++) {
+            String temp = s.substring(idx, i);
+            if (dict.contains(temp) && search(s, dict, i, mem)) {
+                mem[idx] = true;
+                return true;
+            }
+        }
+        mem[idx] = false;
+        return false;
+    }
+}
