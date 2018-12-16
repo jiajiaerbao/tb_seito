2bd9e0775539c5ca9476a0dfd8578a815a789817
seito <ryuuseitou@gmal.com>
2018-12-15 18:52:43 -0800

    做题的时候, 如果遇到难点, 千万不要逃避, 去做其他的是, 一定要继续思考, 把逻辑捋顺了 Everyday is the last day!

--- src/main/java/dfsNormal/L93.java
+++ src/main/java/dfsNormal/L93.java
@@ -3,6 +3,7 @@ package dfsNormal;
 import java.util.ArrayList;
 import java.util.List;
 
+@SuppressWarnings("Duplicates")
 public class L93 {
     /*
      *  坑 1: 坑 BOSS: 拼数思想: 拼的是数字, 因为IP总共能分成四部分数字, 每部分数字最多有三个数字
@@ -10,6 +11,8 @@ public class L93 {
      *  坑 3: 在取 subString 之前, 先判断是否越界, idx+i <= s.length()
      *  坑 4: 在 call 进入下一层 dfs 之后, 还要 set back
      *  坑 5: 加点".": 因为每一次都加点, 最后放入结果集的时候, 要把最后一个点给去掉
+     *  坑 6: IP地址有 4 个点. 不是三个点.
+     *  坑 7: 题目的特殊要求 00. 不符合要求
      * */
     public List<String> restoreIpAddresses(String s) {
         List<String> res = new ArrayList<>();
@@ -49,4 +52,77 @@ public class L93 {
             }
         }
     }
+
+
+
+    /*************************第二遍***********************************/
+    public List<String> restoreIpAddresses2(String s) {
+        List<String> res = new ArrayList<>();
+        dfs(s, 0, new StringBuilder(), 0, res);
+        return res;
+    }
+
+    private void dfs(String s, int idx, StringBuilder path, int cntDot, List<String> res) {
+        if (cntDot == 4 && idx == s.length()) {
+            res.add(path.toString());
+            return;
+        }
+        if (cntDot > 4 || idx >= s.length()) {
+            return;
+        }
+        int size = path.length();
+        for (int i = 1; i <= 3; i++) {
+            if (idx + i > s.length()) {
+                break;
+            }
+            int val = Integer.valueOf(s.substring(idx, idx + i));
+            if (val >= 0 && val < 256) {
+                if (size == 0) {
+                    path.append(val);
+                } else {
+                    path.append(".").append(val);
+                }
+                dfs(s, idx + i, path, cntDot + 1, res);
+                path.setLength(size);
+            }
+            if (val == 0) {
+                break;
+            }
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
