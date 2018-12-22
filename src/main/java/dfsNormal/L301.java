1eb366e23dee7a1d7829e79debca6f21e28b14d3
seito <ryuuseitou@gmal.com>
2018-12-22 10:50:47 -0800

    这道题做了两天, 因为你晚上不做题, 跑出去跟吃饭去了 Everyday is the last day!

--- src/main/java/dfsNormal/L301.java
+++ src/main/java/dfsNormal/L301.java
@@ -11,7 +11,13 @@ public class L301 {
      *       这里左括号个数和右括号个数是 invalid 的括号数, 要保证 minimum 的话, 至少要去掉 所有的invalid左括号的和invalid右括号
      * 坑 2: base failure的情况是: 1.idx越界了; 2.rl:删多了; 2.rr:删多了; 3.delta:不满足条件;
      * 坑 3: 这里只是处理当前层的字符, 不要用for loop去遍历整个字符串, 仅仅是对当前字符而言, 留还是不留;
-     * 坑 4: 结果先存到HashSet里面, 用来去重复, 然后在导入到List里面
+     * 坑 4: 有重复解, 必须去除重复, 用Set: 结果先存到HashSet里面, 用来去重复, 然后在导入到List里面
+     * 坑 5: 结束条件是 idx >= s.length()
+     * 坑 6: 必须记录delta, 否则的话, 无法确定中间过程和最终结果是否满足要求
+     * 坑 7: 坑BOSS: DFS的精髓是只看下一层
+     * 坑 8: input s不仅仅是 ( 和 ), 还有一些其他字符:
+      *         在计算删除最少的 ( 和 )的时候, 要明确判断是否是 ( 和 )
+      *         在dfs里面, 也要判断一些
      * */
     public List<String> removeInvalidParentheses(String s) {
         List<String> res = new ArrayList<>();
@@ -78,4 +84,119 @@ public class L301 {
         }
         return new int[]{rl, rr};
     }
+
+    /*******************************第二遍**********************************/
+    public List<String> removeInvalidParentheses2(String s) {
+        //corner case
+        if (s == null || s.length() == 0) {
+            List<String> res = new ArrayList<>();
+            res.add("");
+            return res;
+        }
+
+        int[] minLR = getMinParentheses(s);
+
+        Set<String> res = new HashSet<>();
+
+        helper(s, minLR[0], minLR[1], 0, res, new StringBuilder(), 0);
+
+        return new ArrayList<>(res);
+    }
+
+    private void helper(String s, int minL, int minR, int delta, Set<String> res, StringBuilder path, int idx) {
+        System.out.println("path: " + path.toString() + ";  idx: " + idx + ";  minL: " + minL + ";  minR: " + minR + ";  delte: " + delta);
+        if (idx == s.length() && minL == 0 && minR == 0 && delta == 0) {
+            res.add(path.toString());
+            return;
+        }
+        if (idx >= s.length() || minL < 0 || minR < 0 || delta < 0) {
+            return;
+        }
+        int size = path.length();
+        char c = s.charAt(idx);
+        if (s.charAt(idx) == '(') {
+            //remove
+            helper(s, minL - 1, minR, delta, res, path, idx + 1);
+            //keep
+            path.append("(");
+            helper(s, minL, minR, delta + 1, res, path, idx + 1);
+            path.setLength(size);
+        } else if (s.charAt(idx) == ')') {
+            //remove
+            helper(s, minL, minR - 1, delta, res, path, idx + 1);
+            //keep
+            path.append(")");
+            helper(s, minL, minR, delta - 1, res, path, idx + 1);
+            path.setLength(size);
+        } else {
+            path.append(s.charAt(idx));
+            helper(s, minL, minR, delta, res, path, idx + 1);
+            path.setLength(size);
+        }
+    }
+
+    private int[] getMinParentheses(String s) {
+        int[] res = new int[2];
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(') {
+                res[0]++;
+            } else if (c == ')') {
+                if (res[0] > 0) {
+                    res[0]--;
+                } else {
+                    res[1]++;
+                }
+            }
+        }
+        return res;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
