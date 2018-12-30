a4493bd34a311c2df865f09b85f62184838aa010
jiajiaerbao <ryuuseitou@gmail.com>
2018-12-30 13:48:41 -0800

    修改了一下base case: 主要是根据状态转移公式的边界来确定 Everyday is the last day!

--- src/main/java/dp/L70.java
+++ src/main/java/dp/L70.java
@@ -3,11 +3,14 @@ package dp;
 
 /*
 * 坑 1: 坑BOSS: 写不出来DP的话, 写DFS+Pruning也可以
-* 坑 2: base case 注意: n=1和n=2都是base case
+* 坑 2: base case 注意:
+*       边界条件的判断就是看状态转移公式里面的 n 有哪些不能取
+*       这里的话, 因为状态转移公式里面有 n-1 和 n-2
+*       所以 n 是 0 和 1 的时候, n-1 和 n-2 是负数, 不能取, 需要在base case里面cover住
+*       最后用n=0,1,2,3来检验一下, n无法达到-1, 因为-1的话, n必须是0或者1, 已经在base case里面了
 * 坑 3: 如果写 dp 的话:
 *       1. 状态转移公式 要明白
 *       2. index的 大小依赖关系 要弄清楚
-*
 * */
 @SuppressWarnings("Duplicates")
 public class L70 {
@@ -16,18 +19,18 @@ public class L70 {
     }
 
     private int helper(int n, int[] mem) {
-        /*if (n == 0) {
-            mem[n] = 0;
+        if (n == 0) {
+            mem[n] = 1;
             return mem[n];
-        }*/
+        }
         if (n == 1) {
             mem[n] = 1;
             return mem[n];
         }
-        if (n == 2) {
+        /*if (n == 2) {
             mem[n] = 2;
             return mem[n];
-        }
+        }*/
         if (mem[n] > 0) {
             return mem[n];
         }
