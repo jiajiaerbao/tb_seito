f9b9df7a7325e1f40e5607a91b6cb99cfda809ab
jiajiaerbao <ryuuseitou@gmail.com>
2018-12-29 22:56:54 -0800

    加了一个 dfs + pruning 的写法, 这种写法的时间复杂度是一样的, 实在不行直接写这种写法 Everyday is the last day!

--- src/main/java/dp/L198.java
+++ src/main/java/dp/L198.java
@@ -1,6 +1,8 @@
 package dp;
 
 
+import java.util.Arrays;
+
 /*
 * 坑 1: 坑BOSS: 搜索问题, 分叉, 还是分叉没搞懂
 *       对于每一个店(点) i: 抢 OR 不抢
@@ -8,10 +10,13 @@ package dp;
 *         不抢: 可以从 i+1 开始
 *       dp[i] = Math.max(dp[i-1], dp[i-2] + nums[i-1])
 * 坑 2: 注意边界条件
+*
 * 坑 3: 根据状态转移公式, 确定 index 的大小依赖关系: 大的依赖于小的
+* 坑 4: 改写成pruning之后, 注意base case, 去掉冗余的base case
 * */
 @SuppressWarnings("Duplicates")
 public class L198 {
+    /*****************************     dp      ***********************/
     public int rob(int[] nums) {
         //corner case
         if (nums == null || nums.length == 0) {
@@ -29,5 +34,34 @@ public class L198 {
             dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
         }
         return dp[nums.length - 1];
+    }
+
+    /*****************************  dfs+pruning  ************************/
+
+    public int rob2(int[] nums) {
+        //corner case
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int[] mem = new int[nums.length];
+        Arrays.fill(mem, -1);
+        return dfs(nums, nums.length - 1, mem);
+    }
+
+    private int dfs(int[] nums, int idx, int[] mem) {
+        if (idx < 0) {
+            return 0;
+        }
+        /*if (idx == 0) {
+            return nums[0];
+        }
+        if (idx == 1) {
+            return Math.max(nums[0], nums[1]);
+        }*/
+        if (mem[idx] != -1) {
+            return mem[idx];
+        }
+        mem[idx] = Math.max(dfs(nums, idx - 1, mem), dfs(nums, idx - 2, mem) + nums[idx]);
+        return mem[idx];
     }
 }
