0ef813ac7ed75c5f9ed3b9bc1828c2ca1c0322b9
jiajiaerbao <ryuuseitou@gmail.com>
2019-02-03 21:33:09 -0800

    你还有十九天, 而你浪费了九天用来放纵自己!!!! 伤害自己的身体, 半夜睡觉!!!! Everyday is the last day!

---
+++ src/main/java/peilian/L21.java
@@ -0,0 +1,23 @@
+package peilian;
+
+/*
+* 坑 1: 这是前面一道题的一部分代码
+* */
+@SuppressWarnings("Duplicates")
+public class L21 {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+}
