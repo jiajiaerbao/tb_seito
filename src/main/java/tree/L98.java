5ed38a92b013e5b214d4e5abb2827636699973db
jiajiaerbao <ryuuseitou@gmail.com>
2019-02-08 08:39:09 -0800

    你还有十一天, 而你浪费了十一天用来放纵自己!!!! 伤害自己的身体, 白天聊天, 半夜睡觉!!!! Everyday is the last day!

--- src/main/java/tree/L98.java
+++ src/main/java/tree/L98.java
@@ -8,25 +8,102 @@ package tree;
 * */
 @SuppressWarnings("Duplicates")
 public class L98 {
-    public boolean isValidBST(TreeNode root) {
+    public boolean isValidBST_1(TreeNode root) {
         if (root == null) {
             return true;
         }
         Integer[] prev = {null};
-        return dfs(root, prev);
+        return dfs_1(root, prev);
     }
 
-    private boolean dfs(TreeNode root, Integer[] prev) {
+    private boolean dfs_1(TreeNode root, Integer[] prev) {
         if (root == null) {
             return true;
         }
-        if (!dfs(root.left, prev)) {
+        if (!dfs_1(root.left, prev)) {
             return false;
         }
         if (prev[0] != null && prev[0] >= root.val) {
             return false;
         }
         prev[0] = root.val;
-        return dfs(root.right, prev);
+        return dfs_1(root.right, prev);
+    }
+    /*
+    *   对右子树, root是Prev
+    *   对root, 左子树是prev
+    *   在touch第二次的时候, 进行比较. 从左子树返回来的时候, 设置prev
+    *   每个节点会被touch三次, 在去右子树之前, 设置prev
+    * */
+    /************************************************************************/
+    public boolean isValidBST(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        Integer[] prev = new Integer[1];
+        return checkBST(root, prev);
+    }
+
+    private boolean checkBST(TreeNode root, Integer[] prev) {
+        if (root == null) {
+            return true;
+        }
+        if (!checkBST(root.left, prev)) {
+            return false;
+        }
+        //在第二次 touch  root 的时候, check 单调递增性
+        if (prev[0] != null && prev[0] >= root.val) {
+            return false;
+        }
+        //每个节点会被touch三次, 在去右子树之前, 设置prev
+        prev[0] = root.val;
+        return checkBST(root.right, prev);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
