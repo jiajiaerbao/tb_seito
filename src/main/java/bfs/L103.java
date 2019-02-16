f62a3a609d67e14a554764b57f4402377e0d5a37
jiajiaerbao <ryuuseitou@gmail.com>
2019-02-16 11:57:56 -0800

    你还有九天, 而你浪费了九天用来放纵自己!!!! 伤害自己的身体, 白天聊天, 半夜睡觉!!!! Everyday is the last day!

--- src/main/java/bfs/L103.java
+++ src/main/java/bfs/L103.java
@@ -1,14 +1,12 @@
 package bfs;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 /**
  * Created by Ryu 11/24/12
  *
  */
+@SuppressWarnings("Duplicates")
 public class L103 {
     class TreeNode {
         int val;
@@ -19,14 +17,50 @@ public class L103 {
             val = x;
         }
     }
-
+    /******************************第二遍********************************************/
+    /*
+    * 坑 1: 这里是充分利用了 BFS 的模板, 然后利用了 Collections.reverse()对某一层进行 reverse
+    * 坑 2: 通过一个flag来控制是否反转
+    * */
+    public List<List<Integer>> zigzagLevelOrder_1(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        boolean flag = false;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> list = new ArrayList<>();
+            while (size-- > 0) {
+                TreeNode curNode = queue.poll();
+                list.add(curNode.val);
+                if (curNode.left != null) {
+                    queue.offer(curNode.left);
+                }
+                if (curNode.right != null) {
+                    queue.offer(curNode.right);
+                }
+            }
+            if (flag) {
+                Collections.reverse(list);
+                res.add(list);
+            } else {
+                res.add(list);
+            }
+            flag = !flag;
+        }
+        return res;
+    }
+    /******************************第一遍********************************************/
     /*
      * 这道题的思维转换点是:
      *      在哪里进行转换方向, 是在queue上面进行转换方向还是在list上面进行转换方向
      *      1. 充分利用list的 add(val) 和 add(0, val)可以从头尾两个方向进行添加
      *      2. 每次只是考虑当前 cur node 的val, 不需要考虑left 和 right 的 val
      * */
-    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+    public List<List<Integer>> zigzagLevelOrder_2(TreeNode root) {
         List<List<Integer>> res = new ArrayList<>();
         //corner case
         if (root == null) {
