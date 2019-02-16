11bfa116dac59f8cc1bc46d36dec14dcdb63817b
jiajiaerbao <ryuuseitou@gmail.com>
2019-02-16 09:24:33 -0800

    你还有九天, 而你浪费了九天用来放纵自己!!!! 伤害自己的身体, 白天聊天, 半夜睡觉!!!! Everyday is the last day!

--- src/main/java/bfs/L102.java
+++ src/main/java/bfs/L102.java
@@ -9,6 +9,8 @@ import java.util.Queue;
  * Created by Ryu 11/23/12
  *
  */
+
+@SuppressWarnings("Duplicates")
 public class L102 {
 
     class TreeNode {
@@ -21,7 +23,36 @@ public class L102 {
         }
     }
 
-    public List<List<Integer>> levelOrder(TreeNode root) {
+/******************************第二遍********************************************/
+    public List<List<Integer>> levelOrder_2(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        Queue<TreeNode> que = new LinkedList<>();
+        que.offer(root);
+        while (!que.isEmpty()) {
+            int size = que.size();
+            List<Integer> list = new ArrayList<>();
+            while (size-- > 0) {
+                TreeNode cur = que.poll();
+                list.add(cur.val);
+                if (cur.left != null) {
+                    que.offer(cur.left);
+                }
+                if (cur.right != null) {
+                    que.offer(cur.right);
+                }
+            }
+            res.add(list);
+        }
+        return res;
+    }
+
+
+
+/******************************第一遍********************************************/
+    public List<List<Integer>> levelOrder_1(TreeNode root) {
         List<List<Integer>> res = new ArrayList<>();
         //corner case
         if(root == null){
