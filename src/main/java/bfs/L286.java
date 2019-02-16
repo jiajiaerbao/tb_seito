1e530e1e8746d593e988dae5fc81e65089b9d294
jiajiaerbao <ryuuseitou@gmail.com>
2019-02-16 12:40:17 -0800

    你还有九天, 而你浪费了九天用来放纵自己!!!! 伤害自己的身体, 白天聊天, 半夜睡觉!!!! Everyday is the last day!

--- src/main/java/bfs/L286.java
+++ src/main/java/bfs/L286.java
@@ -5,7 +5,68 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
+@SuppressWarnings("Duplicates")
 public class L286 {
+    /******************************第二遍********************************************/
+    /*
+    * 坑 1: 关键点是把什么放到Queue里面, 这里是把 Gate 放到Queue里面, 从 Gate 出发, 到所有 room 的距离
+    *       如果实在记不住 哪个值对应着哪个点, 可以用下面这两个来代替
+    *           private static final GATE = 0;
+    *           private static final ROOM = Integer.MAX_VAL;
+    * */
+    public void wallsAndGates_1(int[][] rooms) {
+        //corner case
+        if (rooms == null || rooms.length == 0 || rooms[0] == null || rooms[0].length == 0) {
+            return;
+        }
+        int row = rooms.length;
+        int col = rooms[0].length;
+        boolean[][] visited = new boolean[row][col];
+        int[][] dirs = new int[][]{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+        Queue<int[]> queue = new LinkedList<>();
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (rooms[i][j] == 0) {
+                    queue.offer(new int[]{i, j});
+                    visited[i][j] = true;
+                }
+            }
+        }
+        int dict = 0;
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            while (size-- > 0) {
+                int[] cur = queue.poll();
+                rooms[cur[0]][cur[1]] = dict;
+                /*if (rooms[cur[0]][cur[1]] == Integer.MAX_VALUE) {
+                    rooms[cur[0]][cur[1]] = dict;
+                }*/
+                for (int[] dir : dirs) {
+                    int nextI = cur[0] + dir[0];
+                    int nextJ = cur[1] + dir[1];
+                    if (nextI >= 0 && nextI < row && nextJ >= 0 && nextJ < col &&
+                            rooms[nextI][nextJ] == Integer.MAX_VALUE && !visited[nextI][nextJ]) {
+                        queue.offer(new int[]{nextI, nextJ});
+                        visited[nextI][nextJ] = true;
+                    }
+                }
+            }
+            dict++;
+        }
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (rooms[i][j] == Integer.MAX_VALUE) {
+                    rooms[i][j] = -1;
+                }
+            }
+        }
+    }
+
+
+
+    /******************************第一遍********************************************/
     /*
      * 把什么发到 Queue 里面:
      *   1. 刚开始的时候, 把所有的 gate (0) 放到 queue 里面
@@ -14,7 +75,7 @@ public class L286 {
      * 如何查环:
      *   因为 room (INF) 的初始值是 INF, 当改变 room 的值时候, 就相当于 mark 成已经访问过了
      * */
-    public void wallsAndGates(int[][] rooms) {
+    public void wallsAndGates_2(int[][] rooms) {
         //corner case
         if (rooms == null || rooms.length == 0 || rooms[0] == null || rooms[0].length == 0) {
             //这里需要跟面试官clarify: throw new IllegalArgumentException("Invalid Input!");
