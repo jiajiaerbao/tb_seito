15c2810b407c6eb1ec4a7268942041137a57443c
jiajiaerbao <ryuuseitou@gmail.com>
2019-02-19 14:32:37 -0800

    你还有七天, 而你浪费了七天用来放纵自己!!!! 伤害自己的身体, 白天聊天, 半夜睡觉!!!! Everyday is the last day!

--- src/main/java/stack/L144.java
+++ src/main/java/stack/L144.java
@@ -4,14 +4,40 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
 
-/*
-* 坑 1: 画图说明的时候, 只画出来针对 pre-order/ post-order 的图即可
-* 坑 2: 第一次 touch 到 node A 的时候, 是 A 作为root, 对应于 if, 沿着左子树走下去
-*       第二次 touch 到 node A 的时候, 是 A 弹栈的时候, 对应于 else, 沿着右子树走下去
-* */
 @SuppressWarnings("Duplicates")
 public class L144 {
-    public List<Integer> preorderTraversal_2(TreeNode root) {
+    /******************************第二遍********************************************/
+    /*
+    * 坑 1: 这道题做了三个小时, 因为不理解, 不明白为什么while loop里面这样写
+    * 坑 2: 这道题的while loop一定要写得非常熟练,
+    *       而且要用一个node有三次机会出现在栈顶的方式 画图 解释清楚
+    * */
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur = root;
+        while (cur != null || !stack.isEmpty()) {
+            if (cur != null) {
+                res.add(cur.val);
+                stack.push(cur);
+                cur = cur.left;
+            } else {
+                TreeNode top = stack.pop();
+                cur = top.right;
+            }
+        }
+        return res;
+    }
+    /******************************第一遍********************************************/
+    /*
+     * 坑 1: 画图说明的时候, 只画出来针对 pre-order/ post-order 的图即可
+     * 坑 2: 第一次 touch 到 node A 的时候, 是 A 作为root, 对应于 if, 沿着左子树走下去
+     *       第二次 touch 到 node A 的时候, 是 A 弹栈的时候, 对应于 else, 沿着右子树走下去
+     * */
+    public List<Integer> preorderTraversal_1_2(TreeNode root) {
         List<Integer> res = new ArrayList<>();
         //corner case 可以省略
         //first time touch node put into result list
@@ -33,7 +59,7 @@ public class L144 {
         return res;
     }
 
-    public List<Integer> preorderTraversal_3(TreeNode root) {
+    public List<Integer> preorderTraversal_1_3(TreeNode root) {
         List<Integer> res = new ArrayList<>();
         Stack<TreeNode> stack = new Stack<>();
         TreeNode cur = root;
