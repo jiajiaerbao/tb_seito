29850ab137c95e6cf428c340ae8717e663018da2
jiajiaerbao <ryuuseitou@gmail.com>
2019-02-19 14:40:07 -0800

    你还有七天, 而你浪费了七天用来放纵自己!!!! 伤害自己的身体, 白天聊天, 半夜睡觉!!!! Everyday is the last day!

--- src/main/java/stack/L94.java
+++ src/main/java/stack/L94.java
@@ -4,13 +4,40 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
 
-/*
-* 坑 1: else的部分, 要注意, 用的是 cur = top.right
-*
-* */
+
 @SuppressWarnings("Duplicates")
 public class L94 {
-    public List<Integer> inorderTraversal(TreeNode root) {
+    /******************************第二遍********************************************/
+    /*
+    * 坑 1: 坑 BOSS, 按照everNote上面记的笔记,
+    *       把preOrder和inOrder放在一起考虑, 用一个模板通吃
+    * */
+    public List<Integer> inorderTraversal_2(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur = root;
+        while (cur != null || !stack.isEmpty()) {
+            if (cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            } else {
+                TreeNode top = stack.pop();
+                res.add(top.val);
+                cur = top.right;
+            }
+        }
+        return res;
+    }
+
+    /******************************第一遍********************************************/
+    /*
+     * 坑 1: else的部分, 要注意, 用的是 cur = top.right
+     *
+     * */
+    public List<Integer> inorderTraversal_1(TreeNode root) {
         List<Integer> res = new ArrayList<>();
         TreeNode cur = root;
         Stack<TreeNode> stack = new Stack<>();
