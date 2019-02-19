ad392aac7799411883c3fe34baaf2f7c9bc21a77
jiajiaerbao <ryuuseitou@gmail.com>
2019-02-19 08:48:43 -0800

    你还有七天, 而你浪费了七天用来放纵自己!!!! 伤害自己的身体, 白天聊天, 半夜睡觉!!!! Everyday is the last day!

--- src/main/java/stack/L394.java
+++ src/main/java/stack/L394.java
@@ -2,17 +2,56 @@ package stack;
 
 import java.util.Stack;
 
-/*
-* 坑 1: 处理右括号的逻辑不清晰
-*           从 number stack 里面 pop 出来数字 NUM
-*           从 String stack 里面 pop 出来字符串 STR
-*           用 一个新的 StringBuilder 来生成 重复字符串STR  固定次数NUM 的 字符串
-*
-*           再 append 到 String stack的最上面的字符串上面
-* */
+
+
 @SuppressWarnings("Duplicates")
 public class L394 {
-    public String decodeString(String s) {
+    /******************************第二遍********************************************/
+    /*
+    * 坑 1: 最开始的时候, 要在stackStr里面push一个StringBuilder, 用来接收字符
+    * */
+    public String decodeString_2(String s) {
+        //corner case
+        if (s == null || s.length() == 0) {
+            return "";
+        }
+        Stack<Integer> stackNum = new Stack<>();
+        Stack<StringBuilder> stackStr = new Stack<>();
+        stackStr.push(new StringBuilder());
+        char[] chars = s.toCharArray();
+        int val = 0;
+        final char LEFT = '[';
+        final char RIGHT = ']';
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] == LEFT) {
+                stackNum.push(val);
+                val = 0;
+                stackStr.push(new StringBuilder());
+            } else if (chars[i] == RIGHT) {
+                int cnt = stackNum.pop();
+                String curStr = stackStr.pop().toString();
+                while (cnt > 0) {
+                    stackStr.peek().append(curStr);
+                    cnt--;
+                }
+            } else if (chars[i] >= '0' && chars[i] <= '9') {
+                val = val * 10 + chars[i] - '0';
+            } else {
+                stackStr.peek().append(chars[i]);
+            }
+        }
+        return stackStr.peek().toString();
+    }
+    /******************************第一遍********************************************/
+    /*
+     * 坑 1: 处理右括号的逻辑不清晰
+     *           从 number stack 里面 pop 出来数字 NUM
+     *           从 String stack 里面 pop 出来字符串 STR
+     *           用 一个新的 StringBuilder 来生成 重复字符串STR  固定次数NUM 的 字符串
+     *
+     *           再 append 到 String stack的最上面的字符串上面
+     * */
+    public String decodeString_1(String s) {
         if (s == null || s.length() == 0) {
             return s;
         }
