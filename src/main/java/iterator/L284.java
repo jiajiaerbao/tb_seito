3db95b73fed3f30e483f8d6f945af2e574a8a3b0
jiajiaerbao <ryuuseitou@gmail.com>
2019-02-23 10:10:58 -0800

    你还有三天, 而你浪费了三天用来放纵自己!!!! 伤害自己的身体, 白天聊天, 半夜睡觉!!!! Everyday is the last day!

--- src/main/java/iterator/L284.java
+++ src/main/java/iterator/L284.java
@@ -4,7 +4,44 @@ import java.util.Iterator;
 
 @SuppressWarnings("Duplicates")
 public class L284 {
+    /******************************第二遍********************************************/
+    class SecondTime{
+        class PeekingIterator implements Iterator<Integer> {
+            private Iterator<Integer> iterator;
+            private Integer cur;
 
+            public PeekingIterator(Iterator<Integer> iterator) {
+                // initialize any member here.
+                this.iterator = iterator;
+                this.cur = null;
+                if (this.iterator.hasNext()) {
+                    cur = iterator.next();
+                }
+            }
+
+            // Returns the next element in the iteration without advancing the iterator.
+            public Integer peek() {
+                return cur;
+            }
+
+            // hasNext() and next() should behave the same as in the Iterator interface.
+            // Override them if needed.
+            @Override
+            public Integer next() {
+                Integer temp = cur;
+                cur = null;
+                if (iterator.hasNext()) {
+                    cur = iterator.next();
+                }
+                return temp;
+            }
+
+            @Override
+            public boolean hasNext() {
+                return cur != null || iterator.hasNext();
+            }
+        }
+    }
     /******************************第一遍********************************************/
     /*
     * 坑 1: 这个答案跟算法哥给的答案不一致,
@@ -12,7 +49,7 @@ public class L284 {
     *               也就是 cur 里面存的是下一个将要输出的元素
     *               而每次 call .next() 的时候, 都是先输出cur, 把下一个元素转移到cur里面
     * */
-    class firstTime{
+    class FirstTime{
         class PeekingIterator implements Iterator<Integer> {
             private Integer cur;
             private Iterator<Integer> iterator;
